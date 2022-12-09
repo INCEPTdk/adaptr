e389d0a63351816bba21ee4cdca12708a6d01306
Anders Granholm <anders.granholm@regionh.dk>
2022-12-09 14:04:56 +0100

    Fix error due to devtools and export issue to multiple cores

--- tests/testthat/test-run_trials.R
+++ tests/testthat/test-run_trials.R
@@ -1,20 +1,26 @@
-test_that("dispatch_trial_runs works", {
-  setup <- read_testdata("binom__setup__3_arms__common_control__equivalence__futility__softened")
+# This test commented out
+# Caused an error when a previous version of the adaptr package is installed -
+# functions from the package and not the updated functions from the development
+# version are loaded on the multiple cores. The export solution only solves this
+# when the package is not installed.
 
-  # Serial run
-  expect_snapshot(
-    dispatch_trial_runs(1:5, setup, base_seed = 12345, sparse = FALSE, cores = 1)
-  )
-
-  # Parallel run
-  cl <- parallel::makeCluster(2)
-  on.exit(parallel::stopCluster(cl))
-  # Necessary to avoid testing error due to versions of functions from previous installed package being loaded on the cluster
-  parallel::clusterExport(cl, ls("package:adaptr"))
-  expect_snapshot(
-    dispatch_trial_runs(1:5, setup, base_seed = 12345, sparse = TRUE, cores = 2, cl = cl)
-  )
-})
+# test_that("dispatch_trial_runs works", {
+#   setup <- read_testdata("binom__setup__3_arms__common_control__equivalence__futility__softened")
+#
+#   # Serial run
+#   expect_snapshot(
+#     dispatch_trial_runs(1:5, setup, base_seed = 12345, sparse = FALSE, cores = 1)
+#   )
+#
+#   # Parallel run
+#   cl <- parallel::makeCluster(2)
+#   on.exit(parallel::stopCluster(cl))
+#   # Necessary to avoid testing error due to versions of functions from previous installed package being loaded on the cluster
+#   parallel::clusterExport(cl, ls("package:adaptr"))
+#   expect_snapshot(
+#     dispatch_trial_runs(1:5, setup, base_seed = 12345, sparse = TRUE, cores = 2, cl = cl)
+#   )
+# })
 
 test_that("single trial simulation works", {
   setup <- read_testdata("binom__setup__3_arms__no_control__equivalence__softened")
@@ -54,4 +60,12 @@ test_that("multiple trials simulation works", {
 test_that("prog_breaks", {
   expect_snapshot(prog_breaks(0.1, n_rep_new = 10, cores = 1))
   expect_snapshot(prog_breaks(0.1, n_rep_new = 10, cores = 2))
+})
+
+# This test also uses extract_results, to avoid the issue mentioned at the top
+test_that("multiple trials simulation works on multiple cores", {
+  setup <- read_testdata("binom__setup__3_arms__no_control__equivalence__softened")
+  res_extr <- extract_results(run_trials(setup, n_rep = 20, base_seed = 12345, sparse = FALSE))
+  res_mc_extr <- extract_results(run_trials(setup, n_rep = 20, base_seed = 12345, sparse = FALSE, cores = 2))
+  expect_equal(res_extr, res_mc_extr)
 })
