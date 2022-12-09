f3dfa83f41baf9a808f4fb66d0766286c7059bfa
Anders Granholm <anders.granholm@regionh.dk>
2022-12-09 15:02:16 +0100

    Increase test coverage of plots

--- tests/testthat/test-plot_convergence.R
+++ tests/testthat/test-plot_convergence.R
@@ -7,6 +7,21 @@ test_that("convergence plot of multiple binom trials works", {
   p <- plot_convergence(res, n_split = 2)
   vdiffr::expect_doppelganger("convergence plot, binomial, 2 splits", p)
 
-  p <- plot_convergence(res, metrics = c("prob_conclusive", "prob_superior", "idp"))
+  p <- plot_convergence(res, metrics = c("prob_conclusive", "prob_superior", "idp"), nrow = 3)
   vdiffr::expect_doppelganger("convergence plot, binomial, prob conclusion, prob superior, idp", p)
+
+  p <- plot_convergence(res, metrics = "prob_select_arm_A")
+  vdiffr::expect_doppelganger("convergence plot, binomial, prob select arm A", p)
+})
+
+test_that("convergence plot errors on invalid input", {
+  res <- read_testdata("binom__results__3_arms__common_control__equivalence__futility__softened")
+
+  expect_error(plot_convergence(res, metrics = "wrong metric"))
+  expect_error(plot_convergence(res, metrics = c("size_mean", "size_mean")))
+  expect_error(plot_convergence(res, restrict = "inferior"))
+  expect_error(plot_convergence(res, n_split = 1000))
+  expect_error(plot_convergence(res, resolution = 9.5))
+  expect_error(plot_convergence(res, metrics = "wrong metric"))
+  expect_error(plot_convergence(res, metrics = NULL))
 })
--- tests/testthat/test-plot_history.R
+++ tests/testthat/test-plot_history.R
@@ -25,4 +25,34 @@ test_that("history of single trial works", {
 
   p <- plot_history(res, x_value = "followed n")
   vdiffr::expect_doppelganger("history plot, binomial, single, prob, followed n", p)
+
+  p <- plot_history(res, y_value = "pct")
+  vdiffr::expect_doppelganger("history plot, binomial, single, pct, look", p)
+
+  p <- plot_history(res, y_value = "pct all")
+  vdiffr::expect_doppelganger("history plot, binomial, single, pct all, look", p)
+
+  p <- plot_history(res, y_value = "ratio ys")
+  vdiffr::expect_doppelganger("history plot, binomial, single, ratio ys, look", p)
+
+  p <- plot_history(res, y_value = "ratio ys all")
+  vdiffr::expect_doppelganger("history plot, binomial, single, ratio ys all, look", p)
+
+  p <- plot_history(res, y_value = "n")
+  vdiffr::expect_doppelganger("history plot, binomial, single, n, look", p)
+})
+
+
+test_that("history plot produces errors with invalid input", {
+  result <- read_testdata("binom__result__3_arms__common_control__equivalence__futility__softened")
+  expect_error(plot_history(result, x_value = "n"))
+  expect_error(plot_history(result, y_value = "mean ys"))
+  result <- read_testdata("binom__result__3_arms__no_control__equivalence__softened__sparse")
+  expect_error(plot_history(result)) # error due to sparse object
+
+  results <- read_testdata("binom__results__3_arms__common_control__equivalence__futility__softened")
+  expect_error(plot_history(results, x_value = "n"))
+  expect_error(plot_history(results, y_value = "mean ys"))
+  results <- read_testdata("binom__results__3_arms__no_control__equivalence__softened__sparse")
+  expect_error(plot_history(results)) # error due to sparse object
 })
